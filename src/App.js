bb35727d8e9ea145d0c213e56204140bcbaf526a
venkatesh <dev@five2one.com.au>
2020-07-13 23:09:54 +0530

    feature/addRouter: Configrued react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Header from './components/Header/Header'
+import Help from './components/Help/Help'
+import { BrowserRouter, Route } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import './App.css';
 
 function App() {
   const store = useSelector(state => state);
   return (
+    <BrowserRouter>
     <div className="App">
       <Header />
+      <Route path="/help" component={Help} />
       <br />
       <div>
         {
@@ -18,6 +22,7 @@ function App() {
         }
       </div>
     </div>
+    </BrowserRouter>
   );
 }
 
